52b7e7e9143a9e376662719f8357dbd8239d983d
chenyongyin <wschenyongyin@qq.com>
2016-12-16 08:53:18 +0800

    cyy 12.16

---
+++ app/src/main/java/com/example/kk/arttraining/pay/bean/RemainTimeBean.java
@@ -0,0 +1,8 @@
+package com.example.kk.arttraining.pay.bean;
+
+/**
+ * 作者：wschenyongyin on 2016/12/15 16:02
+ * 说明:
+ */
+public class RemainTimeBean {
+}
---
+++ app/src/main/java/com/example/kk/arttraining/ui/course/bean/LesRecourseBean.java
@@ -0,0 +1,8 @@
+package com.example.kk.arttraining.ui.course.bean;
+
+/**
+ * 作者：wschenyongyin on 2016/12/15 14:15
+ * 说明:
+ */
+public class LesRecourseBean {
+}
