ee581ae9d79d3bf9c7138eb7a862d20709cfc572
kanghuicong <515849594@qq.com>
2016-12-23 15:36:35 +0800

    12.23pull khc

--- app/src/main/java/com/example/kk/arttraining/ui/homePage/activity/ThemeTeacherArtFragment.java
+++ app/src/main/java/com/example/kk/arttraining/ui/homePage/activity/ThemeTeacherArtFragment.java
@@ -122,8 +122,6 @@ public class ThemeTeacherArtFragment extends Fragment implements ITeacherArtSear
     }
 
 
-
-
     @Override
     public void onRefresh(PullToRefreshLayout pullToRefreshLayout) {
 
